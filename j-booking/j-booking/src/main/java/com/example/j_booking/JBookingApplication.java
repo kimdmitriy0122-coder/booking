1c45c2aca0e255ddea07d63c1e05cfb82b5db8f7
Dmitriy Kim <kim.dmitriy.0122@gmail.com>
2026-02-12 18:00:34 +0500

    12.02.2026 18:00

---
+++ j-booking/j-booking/src/main/java/com/example/j_booking/JBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.j_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBookingApplication.class, args);
+	}
+
+}
