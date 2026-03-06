a814a6f838601ce41da8c7bf79cd249dde131770
Dmitriy Kim <kim.dmitriy.0122@gmail.com>
2026-03-06 17:47:18 +0500

    06.03.2026 work

---
+++ j-booking/j-booking/src/main/java/com/example/j_booking/component/scheduler/PaymentStatusChecker.java
@@ -0,0 +1,14 @@
+package com.example.j_booking.component.scheduler;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Component;
+
+//@Component
+//@RequiredArgsConstructor
+//@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class PaymentStatusChecker {
+
+
+}
--- j-booking/j-booking/src/main/java/com/example/j_booking/configuration/WebhookConfig.java
+++ j-booking/j-booking/src/main/java/com/example/j_booking/configuration/WebhookConfig.java
@@ -1,6 +1,5 @@
 package com.example.j_booking.configuration;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
